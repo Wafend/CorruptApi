08dd06901a6fc47d97c9d5ad4df3b66c3fdcd950
Wafend <wafend.business@gmail.com>
2022-08-28 10:04:45 +0200

    added permashow

---
+++ libraries/permaShow.lua
@@ -0,0 +1,19 @@
+---@class permaShow
+local permaShow = {}
+
+---@param key string
+---@param displayName string
+---@param position vec2
+---@return permaShowWindow
+function permaShow.create(key, displayName, position) end
+
+
+---@param key string
+---@return nil
+function permaShow.delete(key) end
+
+
+---@type permaShow
+_G.permaShow = {}
+
+
---
+++ objects/permaShowWindow.lua
@@ -0,0 +1,11 @@
+---@class permaShowWindow
+---@field public visible boolean @can be set
+
+---@param displayName string
+---@param component menuElement
+---@return nil
+function permaShowWindow:add(displayName, component) end
+
+---@type permaShowWindow
+_G.permaShowWindow = {}
+
